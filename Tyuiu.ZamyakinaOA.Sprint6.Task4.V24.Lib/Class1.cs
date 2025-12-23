41f4b0cefa7b4adc52078064b40b1735a0758c86
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-12-24 04:06:03 +0500

    t

--- Tyuiu.ZamyakinaOA.Sprint6.Task4.V24.Lib/Class1.cs
+++ Tyuiu.ZamyakinaOA.Sprint6.Task4.V24.Lib/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using tyuiu.cources.programming.interfaces.Sprint6;
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.ZamyakinaOA.Sprint6.Task4.V24.Lib
 {
@@ -12,20 +6,18 @@ namespace Tyuiu.ZamyakinaOA.Sprint6.Task4.V24.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
             {
-                double[] valueArray;
-                int len = (stopValue - startValue) + 1;
-                valueArray = new double[len];
-                double y;
-                int count = 0;
-                for (int x = startValue; x <= stopValue; x++)
-                {
-                    y = Math.Round(Math.Sin(x) / (x + 1.2) - Math.Sin(x) * 2 - 2 * x, 2);
-                    valueArray[count] = y;
-                    count++;
-                }
-                return valueArray;
+                y = Math.Round(Math.Sin(x) / (x + 1.2) - Math.Sin(x) * 2 - 2 * x, 2);
+                valueArray[count] = y;
+                count++;
             }
+            return valueArray;
         }
     }
 }
