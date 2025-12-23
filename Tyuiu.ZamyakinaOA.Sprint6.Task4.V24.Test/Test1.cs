32fe7e3fdf2866bd794c8efc20bc1dd4b1d51e88
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-12-24 04:09:09 +0500

    t

--- Tyuiu.ZamyakinaOA.Sprint6.Task4.V24.Test/Test1.cs
+++ Tyuiu.ZamyakinaOA.Sprint6.Task4.V24.Test/Test1.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using Tyuiu.ZamyakinaOA.Sprint6.Task4.V24.Lib;
 
 namespace Tyuiu.ZamyakinaOA.Sprint6.Task4.V24.Test
 {
     [TestClass]
-    public class UnitTest1
+    public sealed class Test1
     {
         [TestMethod]
         public void TestMethod1()
