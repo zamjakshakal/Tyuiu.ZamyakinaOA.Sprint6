e38459595bd97f1c0970b60ea5301fa388ab2b84
Олеся Замякина <tyuiu.zamjakinaoa@outlook.com>
2025-12-24 04:18:54 +0500

    y

---
+++ Tyuiu.ZamyakinaOA.Sprint6.Task5.V23.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZamyakinaOA.Sprint6.Task5.V23.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.ZamyakinaOA.Sprint6.Task5.V23/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.ZamyakinaOA.Sprint6.Task5.V23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
